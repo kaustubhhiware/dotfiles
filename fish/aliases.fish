aa234f17b4ba94e1d895795441ce24dac6a036aa
kaustubhhiware <hiwarekaustubh@googlemail.com>
2017-01-13 07:32:31 +0530

    fish screenshot+aliases

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -1,4 +1,5 @@
 alias bandkar "sudo shutdown now"
 alias waapas "sudo reboot"
 alias gst "git status"
+alias netbc " sudo usermod -G netdev -a kaustubh"
 # add install error aliases
--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,13 @@
 . ~/.config/fish/aliases.fish
 
+set fish_greeting ""
+
 set -g -x PATH /usr/local/bin $PATH
 eval (thefuck --alias | tr '\n' ';')
+
+# show system details at terminal start
+# sudo apt-get install screenfetch
+screenfetch 
 
 # modify notifyre compatibility
 # . ~/.notifyre.sh
